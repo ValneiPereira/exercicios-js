8bb6835d618e3d607d161c67c77f0a05cb548ff0
ValneiPereira <valneipereirabarboza@gmail.com>
2020-11-15 23:44:30 -0300

    nullUndefined

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,17 @@
+let valor
+console.log(valor);
+valor = null // ausencia de valor
+console.log(valor);
+const produto = {}
+console.log(produto.preco);
+produto.preco = 3.50
+console.log(produto);
+
+produto.preco = undefined //evite atribuir undefeined
+console.log(!!produto.preco);
+console.log(produto);
+//delete produto.preco
+console.log(produto);
+produto.preco = null //sem preço
+console.log(!!produto.preco);
+console.log(produto);
