a8dcc3729583fbc07f6058dbd4e226301d0b8485
ValneiPereira <valneipereirabarboza@gmail.com>
2020-11-26 22:20:40 -0300

    variaveis_var_let

---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,9 @@
+var numero = 1
+{
+  let numero = 2
+  console.log('dentro =', numero);
+}
+console.log('fore =', numero);
+
+//var = escopo global, escopo de funcao
+//let = escopo global, escopo de funcao e escopo de bloco
