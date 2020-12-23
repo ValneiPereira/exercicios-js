a2e86e8f846b8a405c2d4c0c347119a6a4a7eb73
ValneiPereira <valneipereirabarboza@gmail.com>
2020-12-23 01:53:20 -0300

    callAply

---
+++ funcao/factory.js
@@ -0,0 +1,13 @@
+const prod1 = {
+  nome:'...',
+  preco: 45
+}
+//Factory simples
+function criarPessoa() {
+  return{
+    nome: 'Ana',
+    sobrenome:'Silva'
+  }
+}
+
+console.log(criarPessoa());
