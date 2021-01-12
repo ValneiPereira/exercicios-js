802ed368280e7b8ac17e285eb1febf46141858bc
ValneiPereira <valneipereirabarboza@gmail.com>
2021-01-12 19:27:27 -0300

    map

--- array/foreach2.js
+++ array/foreach2.js
@@ -7,4 +7,5 @@ Array.prototype.forEach2 = function (callback) {
 const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 aprovados.forEach2(function (nome, indice) {
   console.log(`${indice + 1}) ${nome}`);
+
 })
